9501c830a44eb3c30d06bdf4a9a57999f60727ee
admin <vdjalov@gmialcom>
2026-04-01 10:54:27 +0300

    initial push

---
+++ Clerk System/ClercSystem/ClercSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClercSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
