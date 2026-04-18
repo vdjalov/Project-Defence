3a0d007dd16dae2714e3c7d1275aee5d4e71e6d3
admin <vdjalov@gmail.com>
2026-04-19 00:04:49 +0300

    department controller tests initiated

---
+++ Clerk System/ClercSystem/ClercSystem.Tests/DepartmentControllerTests/DepartmentControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using ClercSystem.Controllers;
+using ClercSystem.Services.Interfaces;
+using Moq;
+
+namespace ClercSystem.Tests.DepartmentControllerTests
+{
+    public class DepartmentControllerTests
+    {
+        private Mock<IDepartmentService> departmentServiceMock;
+        private DepartmentController controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.departmentServiceMock = new Mock<IDepartmentService>();
+            this.controller = new DepartmentController(departmentServiceMock.Object);
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.controller?.Dispose();
+        }
+    }
+}
